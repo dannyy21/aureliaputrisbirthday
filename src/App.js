2ab97479414e78249a127e41e2690b70f03a88cc
Danny <dannyptr21@gmail.com>
2024-08-15 11:45:47 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
 import Intro from './components/files/intro';
 import Home from './components/files/home';
 import Introduction from './components/files/introduction';
